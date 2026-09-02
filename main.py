fde8717711a7eeb5ff4d27d07717f4e63956497a
Avinash Aldhapati <130386277+deep0ctave@users.noreply.github.com>
2026-09-02 21:47:32 +0530

    chore: setup basic project template

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Date : 02/09/2026
+Author : Avinash(deep0ctave)
+Purpose : This is a ray-tracing project built from scratch to learn how it works. The project is built using Python and uses the Pygame library for rendering the final image. The project is inspired by the book "Ray Tracing in One Weekend" by Peter Shirley.
+"""
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
