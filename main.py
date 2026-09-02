4c8802bb35b680021a626a62189a623e5cce3812
Avinash Aldhapati <130386277+deep0ctave@users.noreply.github.com>
2026-09-02 22:03:34 +0530

    feat: add feature to generate a ppm file

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ Purpose : This is a ray-tracing project built from scratch to learn how it works
 """
 
 def main():
-    print("Hello, World!")
+
+    # Image Generation
+    image_width = 256
+    image_height = 256
+
+    # Initialize a ppm file
+    print("P3")
+    print(f"{image_width} {image_height}")
+    print("255")
+
+    for y in range (0, image_height):
+        for x in range(0, image_width):
+            r = x / (image_width - 1)
+            g = y / (image_height - 1)
+            b = 0
+
+            print(f"{int(r * 255)} {int(g * 255)} {int(b * 255)}")
 
 if __name__ == "__main__":
     main()
